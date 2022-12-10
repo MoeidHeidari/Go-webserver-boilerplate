603120c86da2985678a6d04a5f960ad7df98d8c6
moeidheidari <moeidheidari@comfortel.pro>
2022-12-10 17:44:27 +0300

    clean arch folder structure finished

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func testApi(c *gin.Context) {
+
+	c.JSON(200, gin.H{
+		"data": "test api works",
+	})
+
+}
 func main() {
-	fmt.Println("hello skyfarm")
+	engine := gin.New()
+	engine.GET("/test", testApi)
+	err := engine.Run("localhost:4000")
+	if err != nil {
+		return
+	}
+
 }
